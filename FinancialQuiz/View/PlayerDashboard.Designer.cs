c980579c3f0030594e7b556d10f4777e66e17876
Kyle Lee <klee25@my.westga.edu>
2021-06-07 12:41:16 -0400

    Added some UI elements to Quiz interface. Removed tabcontrol per meeting notes

--- FinancialQuiz/View/PlayerDashboard.Designer.cs
+++ FinancialQuiz/View/PlayerDashboard.Designer.cs
@@ -29,67 +29,200 @@ namespace FinancialQuiz.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.PlayerDashTabControl = new System.Windows.Forms.TabControl();
-            this.QuizTab = new System.Windows.Forms.TabPage();
-            this.StatsTab = new System.Windows.Forms.TabPage();
-            this.PlayerDashTabControl.SuspendLayout();
+            this.HelloLabel = new System.Windows.Forms.Label();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.LogoutLink = new System.Windows.Forms.LinkLabel();
+            this.CategoryHeaderLabel = new System.Windows.Forms.Label();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.QuestionProgressLabel = new System.Windows.Forms.Label();
+            this.QuestionNumberLabel = new System.Windows.Forms.Label();
+            this.QuestionLabel = new System.Windows.Forms.Label();
+            this.RadioAnswerA = new System.Windows.Forms.RadioButton();
+            this.RadioAnswerB = new System.Windows.Forms.RadioButton();
+            this.RadioAnswerC = new System.Windows.Forms.RadioButton();
+            this.RadioAnswerD = new System.Windows.Forms.RadioButton();
+            this.SubmitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // PlayerDashTabControl
+            // HelloLabel
             // 
-            this.PlayerDashTabControl.Controls.Add(this.QuizTab);
-            this.PlayerDashTabControl.Controls.Add(this.StatsTab);
-            this.PlayerDashTabControl.Location = new System.Drawing.Point(2, 3);
-            this.PlayerDashTabControl.Name = "PlayerDashTabControl";
-            this.PlayerDashTabControl.SelectedIndex = 0;
-            this.PlayerDashTabControl.Size = new System.Drawing.Size(1118, 625);
-            this.PlayerDashTabControl.TabIndex = 0;
+            this.HelloLabel.AutoSize = true;
+            this.HelloLabel.Location = new System.Drawing.Point(907, 19);
+            this.HelloLabel.Name = "HelloLabel";
+            this.HelloLabel.Size = new System.Drawing.Size(31, 13);
+            this.HelloLabel.TabIndex = 0;
+            this.HelloLabel.Text = "Hello";
             // 
-            // QuizTab
+            // UserNameLabel
             // 
-            this.QuizTab.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.QuizTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.QuizTab.Location = new System.Drawing.Point(4, 22);
-            this.QuizTab.Name = "QuizTab";
-            this.QuizTab.Padding = new System.Windows.Forms.Padding(3);
-            this.QuizTab.Size = new System.Drawing.Size(1110, 599);
-            this.QuizTab.TabIndex = 0;
-            this.QuizTab.Text = "Quiz                   ";
-            this.QuizTab.UseVisualStyleBackColor = true;
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Location = new System.Drawing.Point(944, 19);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(126, 13);
+            this.UserNameLabel.TabIndex = 1;
+            this.UserNameLabel.Text = "XXXXXXXXXXXXXXXXX";
             // 
-            // StatsTab
+            // LogoutLink
             // 
-            this.StatsTab.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.StatsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StatsTab.Location = new System.Drawing.Point(4, 22);
-            this.StatsTab.Name = "StatsTab";
-            this.StatsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.StatsTab.Size = new System.Drawing.Size(1110, 599);
-            this.StatsTab.TabIndex = 1;
-            this.StatsTab.Text = "My Stats                      ";
-            this.StatsTab.UseVisualStyleBackColor = true;
+            this.LogoutLink.AutoSize = true;
+            this.LogoutLink.Location = new System.Drawing.Point(1030, 45);
+            this.LogoutLink.Name = "LogoutLink";
+            this.LogoutLink.Size = new System.Drawing.Size(40, 13);
+            this.LogoutLink.TabIndex = 2;
+            this.LogoutLink.TabStop = true;
+            this.LogoutLink.Text = "Logout";
+            // 
+            // CategoryHeaderLabel
+            // 
+            this.CategoryHeaderLabel.AutoSize = true;
+            this.CategoryHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoryHeaderLabel.Location = new System.Drawing.Point(14, 42);
+            this.CategoryHeaderLabel.Name = "CategoryHeaderLabel";
+            this.CategoryHeaderLabel.Size = new System.Drawing.Size(79, 16);
+            this.CategoryHeaderLabel.TabIndex = 3;
+            this.CategoryHeaderLabel.Text = "Category: ";
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoryLabel.Location = new System.Drawing.Point(90, 42);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(162, 16);
+            this.CategoryLabel.TabIndex = 4;
+            this.CategoryLabel.Text = "xxxxxxxxxxxxxxxxxxxxxx";
+            // 
+            // QuestionProgressLabel
+            // 
+            this.QuestionProgressLabel.AutoSize = true;
+            this.QuestionProgressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionProgressLabel.Location = new System.Drawing.Point(268, 42);
+            this.QuestionProgressLabel.Name = "QuestionProgressLabel";
+            this.QuestionProgressLabel.Size = new System.Drawing.Size(29, 16);
+            this.QuestionProgressLabel.TabIndex = 5;
+            this.QuestionProgressLabel.Text = "0/0";
+            // 
+            // QuestionNumberLabel
+            // 
+            this.QuestionNumberLabel.AutoSize = true;
+            this.QuestionNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionNumberLabel.Location = new System.Drawing.Point(96, 156);
+            this.QuestionNumberLabel.Name = "QuestionNumberLabel";
+            this.QuestionNumberLabel.Size = new System.Drawing.Size(25, 24);
+            this.QuestionNumberLabel.TabIndex = 6;
+            this.QuestionNumberLabel.Text = "0.";
+            // 
+            // QuestionLabel
+            // 
+            this.QuestionLabel.AutoSize = true;
+            this.QuestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionLabel.Location = new System.Drawing.Point(127, 156);
+            this.QuestionLabel.Name = "QuestionLabel";
+            this.QuestionLabel.Size = new System.Drawing.Size(267, 24);
+            this.QuestionLabel.TabIndex = 7;
+            this.QuestionLabel.Text = "Using a credit card is most like:";
+            // 
+            // RadioAnswerA
+            // 
+            this.RadioAnswerA.AutoSize = true;
+            this.RadioAnswerA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioAnswerA.Location = new System.Drawing.Point(100, 236);
+            this.RadioAnswerA.Name = "RadioAnswerA";
+            this.RadioAnswerA.Size = new System.Drawing.Size(130, 28);
+            this.RadioAnswerA.TabIndex = 8;
+            this.RadioAnswerA.TabStop = true;
+            this.RadioAnswerA.Text = "A. Answer 1";
+            this.RadioAnswerA.UseVisualStyleBackColor = true;
+            // 
+            // RadioAnswerB
+            // 
+            this.RadioAnswerB.AutoSize = true;
+            this.RadioAnswerB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioAnswerB.Location = new System.Drawing.Point(100, 304);
+            this.RadioAnswerB.Name = "RadioAnswerB";
+            this.RadioAnswerB.Size = new System.Drawing.Size(129, 28);
+            this.RadioAnswerB.TabIndex = 9;
+            this.RadioAnswerB.TabStop = true;
+            this.RadioAnswerB.Text = "B. Answer 2";
+            this.RadioAnswerB.UseVisualStyleBackColor = true;
+            // 
+            // RadioAnswerC
+            // 
+            this.RadioAnswerC.AutoSize = true;
+            this.RadioAnswerC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioAnswerC.Location = new System.Drawing.Point(100, 379);
+            this.RadioAnswerC.Name = "RadioAnswerC";
+            this.RadioAnswerC.Size = new System.Drawing.Size(130, 28);
+            this.RadioAnswerC.TabIndex = 10;
+            this.RadioAnswerC.TabStop = true;
+            this.RadioAnswerC.Text = "C. Answer 3";
+            this.RadioAnswerC.UseVisualStyleBackColor = true;
+            // 
+            // RadioAnswerD
+            // 
+            this.RadioAnswerD.AutoSize = true;
+            this.RadioAnswerD.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioAnswerD.Location = new System.Drawing.Point(100, 454);
+            this.RadioAnswerD.Name = "RadioAnswerD";
+            this.RadioAnswerD.Size = new System.Drawing.Size(130, 28);
+            this.RadioAnswerD.TabIndex = 11;
+            this.RadioAnswerD.TabStop = true;
+            this.RadioAnswerD.Text = "D. Answer 4";
+            this.RadioAnswerD.UseVisualStyleBackColor = true;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(499, 524);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(138, 47);
+            this.SubmitButton.TabIndex = 12;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
             // 
             // PlayerDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1119, 629);
-            this.Controls.Add(this.PlayerDashTabControl);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.RadioAnswerD);
+            this.Controls.Add(this.RadioAnswerC);
+            this.Controls.Add(this.RadioAnswerB);
+            this.Controls.Add(this.RadioAnswerA);
+            this.Controls.Add(this.QuestionLabel);
+            this.Controls.Add(this.QuestionNumberLabel);
+            this.Controls.Add(this.QuestionProgressLabel);
+            this.Controls.Add(this.CategoryLabel);
+            this.Controls.Add(this.CategoryHeaderLabel);
+            this.Controls.Add(this.LogoutLink);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.HelloLabel);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1135, 668);
             this.MinimumSize = new System.Drawing.Size(1135, 668);
             this.Name = "PlayerDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PlayerDashboard";
-            this.PlayerDashTabControl.ResumeLayout(false);
+            this.Text = "Kiddie Cash Komprehension";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl PlayerDashTabControl;
-        private System.Windows.Forms.TabPage QuizTab;
-        private System.Windows.Forms.TabPage StatsTab;
+        private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.LinkLabel LogoutLink;
+        private System.Windows.Forms.Label CategoryHeaderLabel;
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.Label QuestionProgressLabel;
+        private System.Windows.Forms.Label QuestionNumberLabel;
+        private System.Windows.Forms.Label QuestionLabel;
+        private System.Windows.Forms.RadioButton RadioAnswerA;
+        private System.Windows.Forms.RadioButton RadioAnswerB;
+        private System.Windows.Forms.RadioButton RadioAnswerC;
+        private System.Windows.Forms.RadioButton RadioAnswerD;
+        private System.Windows.Forms.Button SubmitButton;
     }
 }
