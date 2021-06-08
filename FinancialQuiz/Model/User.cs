8b3c5ac6583e7a065bee70751fa715e65c510108
Sofiya Anikina <sanikin1@my.westga.edu>
2021-06-08 16:57:46 -0500

    Added Model class for User

---
+++ FinancialQuiz/Model/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialQuiz.Model
+{
+    /// <summary>
+    /// Model class for User object
+    /// </summary>
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string AdminInd { get; set; }
+    }
+}
