393bedf35a971630115ef48f102d63b8ae089c1d
sivanathp <speddak1@my.westga.edu>
2021-06-10 15:00:03 -0500

    updated connection string

--- FinancialQuiz/DAL/DBConnection.cs
+++ FinancialQuiz/DAL/DBConnection.cs
@@ -9,13 +9,13 @@ namespace FinancialQuiz.DAL
         /// </summary>
         public static SqlConnection GetConnection()
         {
-            string connectionString =
-                "Data Source=localhost;Initial Catalog=CS6920-Team4;" +
-                "Integrated Security=True";
-
             //string connectionString =
-            //     "Server = tcp:cs6920.database.windows.net,1433; Initial Catalog = CS6920 - Team4; Persist Security Info = False; User ID = cs6920; Password =P@ssw0rd; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
-           
+            //    "Data Source=localhost;Initial Catalog=CS6920-Team4;" +
+            //    "Integrated Security=True";
+
+            string connectionString =
+                 "Server=tcp:cs6920.database.windows.net,1433;Initial Catalog=CS6920-Team4;Persist Security Info = False; User ID = cs6920; Password =P@ssw0rd; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+                 
 
 
             SqlConnection connection = new SqlConnection(connectionString);
