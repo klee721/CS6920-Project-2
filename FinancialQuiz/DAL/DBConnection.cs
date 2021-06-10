d923edc6d2427f29bb1d8b3a3ce6a97ce30d2b22
sivanathp <speddak1@my.westga.edu>
2021-06-10 01:23:40 -0500

    Created Cloud database and added the connection string in the DBConnection file

--- FinancialQuiz/DAL/DBConnection.cs
+++ FinancialQuiz/DAL/DBConnection.cs
@@ -13,6 +13,10 @@ namespace FinancialQuiz.DAL
                 "Data Source=localhost;Initial Catalog=CS6920-Team4;" +
                 "Integrated Security=True";
 
+            //string connectionString =
+            //     "Server = tcp:cs6920.database.windows.net,1433; Initial Catalog = CS6920 - Team4; Persist Security Info = False; User ID = cs6920; Password =P@ssw0rd; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+           
+
 
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
