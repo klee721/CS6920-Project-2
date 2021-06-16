bf8b2c2d61873f0b1122dcc4d447f9bd0dfe77fb
Kyle Lee <klee25@my.westga.edu>
2021-06-15 23:13:57 -0400

    Added Model tests for User and question, updated Solution to include testing project

---
+++ FinancialQuizTests1/Model/QuestionModelTests.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FinancialQuiz.Model;
+
+namespace FinancialQuizTests1.Model
+{
+    /// <summary>
+    /// Summary description for QuestionModelTests
+    /// </summary>
+    [TestClass]
+    public class QuestionModelTests
+    {
+        public QuestionModelTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestQuestionCreation()
+        {
+            Question testQuestion = new Question();
+            Assert.IsNotNull(testQuestion);
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetID()
+        {
+            Question testQuestion = new Question();
+            testQuestion.QuestionID = 1;
+            Assert.AreEqual(testQuestion.QuestionID, 1);
+
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetDescription()
+        {
+            Question testQuestion = new Question();
+            testQuestion.Description = "How many cents in a dollar?";
+            Assert.AreEqual(testQuestion.Description, "How many cents in a dollar?");
+
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetOptions()
+        {
+            Question testQuestion = new Question();
+            testQuestion.OptionA = "A";
+            Assert.AreEqual(testQuestion.OptionA, "A");
+            testQuestion.OptionB = "B";
+            Assert.AreEqual(testQuestion.OptionB, "B");
+            testQuestion.OptionC = "C";
+            Assert.AreEqual(testQuestion.OptionC, "C");
+            testQuestion.OptionD = "D";
+            Assert.AreEqual(testQuestion.OptionD, "D");
+
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetCorrectOption()
+        {
+            Question testQuestion = new Question();
+            testQuestion.CorrectOption = "A";
+            Assert.AreEqual(testQuestion.CorrectOption, "A");
+
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetAgeRangeID()
+        {
+            Question testQuestion = new Question();
+            testQuestion.AgeRangeID = "1-10";
+            Assert.AreEqual(testQuestion.AgeRangeID, "1-10");
+
+        }
+
+
+        [TestMethod]
+        public void TestQuestionGetSetCategoryID()
+        {
+            Question testQuestion = new Question();
+            testQuestion.CategoryID = 9;
+            Assert.AreEqual(testQuestion.CategoryID, 9);
+
+        }
+
+        [TestMethod]
+        public void TestQuestionGetSetGameLevelID()
+        {
+            Question testQuestion = new Question();
+            testQuestion.GameLevelID = 9;
+            Assert.AreEqual(testQuestion.GameLevelID, 9);
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ FinancialQuizTests1/Model/UserModelTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FinancialQuiz.Model;
+
+namespace FinancialQuizTests1.Model
+{
+    /// <summary>
+    /// Summary description for UserModelTests
+    /// </summary>
+    [TestClass]
+    public class UserModelTests
+    {
+        public UserModelTests()
+        {
+          
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestUserCreation()
+        {
+
+            User testUser = new User();
+            Assert.IsNotNull(testUser);
+           
+        }
+
+        [TestMethod]
+        public void TestUserGetSetID()
+        {
+
+            User testUser = new User();
+            testUser.UserID = 1;
+            Assert.AreEqual(testUser.UserID, 1);
+
+        }
+
+        [TestMethod]
+        public void TestUserGetSetName()
+        {
+
+            User testUser = new User();
+            testUser.FirstName = "Tom";
+            testUser.LastName = "Brady";
+            Assert.AreEqual(testUser.FirstName + testUser.LastName, "TomBrady");
+
+        }
+
+        [TestMethod]
+        public void TestUserGetSetAge()
+        {
+
+            User testUser = new User();
+            testUser.Age = 18;
+            Assert.AreEqual(testUser.Age, 18);
+
+        }
+
+        [TestMethod]
+        public void TestUserGetSetUserName()
+        {
+
+            User testUser = new User();
+            testUser.UserName = "Kyle";
+            Assert.AreEqual(testUser.UserName, "Kyle");
+
+        }
+
+        [TestMethod]
+        public void TestUserGetSetPassword()
+        {
+
+            User testUser = new User();
+            testUser.Password = "test123";
+            Assert.AreEqual(testUser.Password, "test123");
+
+        }
+
+        [TestMethod]
+        public void TestUserGetSetAdminInd()
+        {
+
+            User testUser = new User();
+            testUser.AdminInd = "Y";
+            Assert.AreEqual(testUser.AdminInd, "Y");
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
