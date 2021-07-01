2e95b0d3a61b0acb1f283bd1f335ac2cfade1f3d
sivanathp <speddak1@my.westga.edu>
2021-06-30 23:52:54 -0500

    updated userQuestionAnswer

--- FinancialQuiz/DB Scripts/UpdateUserQuestionAnswer.sql
+++ FinancialQuiz/DB Scripts/UpdateUserQuestionAnswer.sql
@@ -1,13 +1,17 @@
-CREATE PROCEDURE UpdateUserQuestionAnswer
+CREATE PROCEDURE [dbo].[UpdateUserQuestionAnswer]
  @gameId INT,   
  @questionId INT,
- @userOption varchar(1)	
+ @userOption varchar(1)
     
 AS   
 BEGIn
+DECLARE @correctCount INT;
+SET @correctCount = (SELECT count(1) from Questions where id = @questionId and Correct_Option = @userOption);
 
+ 
   
-update GameDetails set userOption =  @userOption where gameID = @gameId and QuestionID = @questionId;
+update GameDetails set userOption =  @userOption  where gameID = @gameId and QuestionID = @questionId;
+update Games set questionsCorrect =  questionsCorrect + @correctCount  where ID = @gameId;
 
 select userOption from GameDetails where gameID = @gameId and QuestionID = @questionId;
   
