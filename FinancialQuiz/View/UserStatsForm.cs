3fef107afaad900668605a087566662934ee834d
sivanathp <speddak1@my.westga.edu>
2021-07-05 22:04:12 -0500

    added changes to report publsh

--- FinancialQuiz/View/UserStatsForm.cs
+++ FinancialQuiz/View/UserStatsForm.cs
@@ -30,9 +30,23 @@ namespace FinancialQuiz.View
 
         private void UserStatsForm_Load(object sender, EventArgs e)
         {
+            var reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+
+            reportViewer1.LocalReport.ReportEmbeddedResource = "FinancialQuiz.View.UserReport.rdlc";
+
             this.getUserReportTableAdapter.Fill(this.userStatsDataSet.GetUserReport, this.loggedInUser.UserID);
             this.reportViewer1.RefreshReport();
-            this.reportViewer1.RefreshReport();
+
+
+            //reportViewer1.Reset();
+           // reportViewer1.LocalReport.ReportEmbeddedResource = "FinancialQuiz.View.UserReport.rdlc";
+           // Microsoft.Reporting.WinForms.ReportDataSource reportDataSource = new Microsoft.Reporting.WinForms.ReportDataSource();
+            // Must match the DataSet in the RDLC
+            //reportDataSource.Name = "UserStatsReportDataSet";
+            //reportDataSource.Value = userStatsDataSet.Tables[0];
+            //reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+            //reportViewer1.RefreshReport();
+
         }
     }
 }
