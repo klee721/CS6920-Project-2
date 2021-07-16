42d115d9ea9de3b529c387052008ccfadb6a1452
sivanathp <speddak1@my.westga.edu>
2021-07-16 16:04:30 -0500

    modified report

--- FinancialQuiz/View/AdminDashboard.Designer.cs
+++ FinancialQuiz/View/AdminDashboard.Designer.cs
@@ -73,22 +73,14 @@ namespace FinancialQuiz.View
             this.UsersTab.Controls.Add(this.manageUserControl1);
             this.UsersTab.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.UsersTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< Updated upstream
             this.UsersTab.Location = new System.Drawing.Point(4, 32);
             this.UsersTab.Margin = new System.Windows.Forms.Padding(4);
             this.UsersTab.Name = "UsersTab";
             this.UsersTab.Padding = new System.Windows.Forms.Padding(4);
             this.UsersTab.Size = new System.Drawing.Size(1483, 1164);
-=======
-            this.UsersTab.Location = new System.Drawing.Point(4, 30);
-            this.UsersTab.Name = "UsersTab";
-            this.UsersTab.Padding = new System.Windows.Forms.Padding(3);
-            this.UsersTab.Size = new System.Drawing.Size(1110, 941);
->>>>>>> Stashed changes
             this.UsersTab.TabIndex = 1;
             this.UsersTab.Text = "Users                                 ";
             // 
-<<<<<<< Updated upstream
             // manageUserControl1
             // 
             this.manageUserControl1.BackColor = System.Drawing.SystemColors.ControlLight;
@@ -99,8 +91,6 @@ namespace FinancialQuiz.View
             this.manageUserControl1.Size = new System.Drawing.Size(1112, 782);
             this.manageUserControl1.TabIndex = 0;
             // 
-=======
->>>>>>> Stashed changes
             // LogoutLink
             // 
             this.LogoutLink.AutoSize = true;
